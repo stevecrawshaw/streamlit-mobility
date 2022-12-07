c74156d5be232c253ef894cc0d48dc7e4ac4945b
Steve Crawshaw <steve.crawshaw@bristol.gov.uk>
2022-12-07 19:17:21 +0000

    Update streamlit_mobility.py

--- streamlit_mobility.py
+++ streamlit_mobility.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 
 mob_data = pd.read_csv("applemobilitytrends-2020-04-19.csv" )
 
-city = "London"
+city = st.sidebar.selectbox("Select a City", ["London", "Paris"])
+
+mode = st.sidebar.selectbox("Select mode", ["driving", "walking"])
+
 city_data = mob_data[mob_data["region"] == city] \
 .drop(columns = ["geo_type", "region"]) \
 .melt(id_vars = "transportation_type", var_name = "date", value_name = "flow")
 
 city_data['date'] = pd.to_datetime(city_data['date'])
 
-mode = "driving"
+# mode = "driving"
 
 mode_filtered_df = city_data[city_data["transportation_type"] == mode]
 
@@ -19,6 +22,5 @@ plot_df = mode_filtered_df.set_index("date") \
 .drop(columns = "transportation_type")
 
 plt.style.use("fivethirtyeight")
-
 plt.xticks([])
-plt.plot(plot_df);
+st.write(plt.plot(plot_df))
